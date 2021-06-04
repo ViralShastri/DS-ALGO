e0b65dce66652e9c45fabf71872ccce3d0c9fa67
Viral Shsatri <shastriv19@gmail.com>
2021-06-04 16:16:58 +0530

    15_FourNumberSum

--- Arrays/15_FourNumberSum.py
+++ Arrays/15_FourNumberSum.py
@@ -1,48 +1,33 @@
 '''
-Linear Approch
+Quadratic Approch
 --------------
-Time - O(N) - Big N - Traversal of each Ele in Matrix
-Space - O(N)
+Time - O(N^2) Worst - O(N^3)
+Space - O(N^2)
 '''
 
 
-def SpiralTraverse(matrix):
-    sRow = 0
-    sCol = 0
-    eRow = len(matrix) - 1
-    eCol = len(matrix[0]) - 1
+def fourNumberSum(nums, target):
+    hashTable = {}
     result = []
 
-    while sRow <= eRow and sCol <= eCol:
-        for c in range(sCol, eCol + 1):
-            result.append(matrix[sRow][c])
+    for i in range(1, len(nums) - 1):
+        for j in range(i+1, len(nums)):
+            currentSum = nums[i] + nums[j]
+            difference = target - currentSum
 
-        for r in range(sRow + 1, eRow + 1):
-            result.append(matrix[r][eCol])
+            if difference in hashTable:
+                for pair in hashTable[difference]:
+                    result.append(pair + [nums[i], nums[j]])
 
-        for c in reversed(range(sCol, eCol)):
-            if sRow == eRow:
-                break
-            result.append(matrix[eRow][c])
-
-        for r in reversed(range(sRow + 1, eRow)):
-            if sCol == eCol:
-                break
-            result.append(matrix[r][sCol])
-
-        sRow += 1
-        sCol += 1
-        eRow -= 1
-        eCol -= 1
+        for k in range(i):
+            currentSum = nums[k] + nums[i]
+            if currentSum not in hashTable:
+                hashTable[currentSum] = []
+            hashTable[currentSum].append([nums[k], nums[i]])
 
     return result
 
 
-matrix = [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]]
-print(SpiralTraverse(matrix))
-
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(SpiralTraverse(matrix))
-
-matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
-print(SpiralTraverse(matrix))
+nums = [7, 6, 4, -1, 1, 2]
+target = 16
+print(fourNumberSum(nums, target))
