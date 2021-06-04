485451128b866c66f2fbd75e67b75a6437b72b56
Viral Shsatri <shastriv19@gmail.com>
2021-06-04 15:44:15 +0530

    13_FirstDuplicateValue

--- Arrays/13_FirstDuplicateValue.py
+++ Arrays/13_FirstDuplicateValue.py
@@ -1,20 +1,41 @@
 '''
 Linear Approch
 --------------
-Time - O(N) 
+Time - O(N)
 Space - O(N)
 '''
 
 
+# def firstDuplicateValue(nums):
+#     hashTable = {}
+#     for num in nums:
+#         if num in hashTable:
+#             return num
+#         hashTable[num] = 1
+#     return -1
+
+
+'''
+Optimal Approch
+--------------
+Time - O(N)
+Space - O(1)
+'''
+
+
 def firstDuplicateValue(nums):
-    pass
+    for num in nums:
+        if nums[abs(num) - 1] < 0:
+            return abs(num)
+        nums[abs(num) - 1] *= -1
+    return -1
 
 
-nums = []
+nums = [2, 1, 5, 2, 3, 3, 4]
 print(firstDuplicateValue(nums))
 
-nums = []
+nums = [2, 1, 5, 3, 3, 2]
 print(firstDuplicateValue(nums))
 
-nums = []
+nums = [1, 2, 3, 4, 5, 6]
 print(firstDuplicateValue(nums))
